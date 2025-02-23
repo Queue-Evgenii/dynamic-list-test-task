8b34a25e50ae445570c03f930f10f487d6c2fdd3
Queue-Evgenii <genadolgii228@gmail.com>
2025-02-23 10:28:41 +0200

    ADD search UI

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="container">
+  <div class="container _flex _fd-col _gap-y-16">
+    <search-component @update:query="searchHandler" />
     <list-component :list="products" />
   </div>
 </template>
@@ -7,17 +8,20 @@
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import ListComponent from "@/components/list/ListComponent.vue";
-import { getProducts } from "./api/products";
-import { Product } from "./interfaces/product";
+import { getProducts } from "@/api/products";
+import { Product } from "@/interfaces/product";
 
 import "@/styles/reset.css";
 import "@/styles/main.css";
 import "@/styles/flex.css";
+import SearchComponent from "@/components/search/SearchComponent.vue";
+import { searchProducts } from "@/api/search";
 
 export default defineComponent({
   name: "App",
   components: {
     ListComponent,
+    SearchComponent,
   },
   setup() {
     const products = ref<Product[]>([]);
@@ -25,8 +29,15 @@ export default defineComponent({
       .then((data) => (products.value = data.products))
       .catch((err) => console.log("getProducts ERR", err));
 
+    const searchHandler = (value: string) => {
+      searchProducts(value)
+        .then((data) => (products.value = data.products))
+        .catch((err) => console.log("searchProducts ERR", err));
+    };
+
     return {
       products,
+      searchHandler,
     };
   },
 });
